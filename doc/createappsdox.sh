c4cde92ea41016eb6bee350875fe22a0942eca5c
user <user@osgeolive.(none)>
2013-02-05 04:44:17 +0100

    worked on documentation in petit Paris

--- doc/createappsdox.sh
+++ doc/createappsdox.sh
@@ -33,14 +33,22 @@ for file in ../src/apps/pk*.cc;do
     THELONGDESCRIPTION="$THESHORTDESCRIPTION more..."
     sed -i "s/thelongdescription/$THELONGDESCRIPTION/" ../doc/${THETOOL}.dox
     ${THETOOL} --doxygen|sed '$d' >> ../doc/${THETOOL}.dox
+    echo >> ../doc/${THETOOL}.dox
+    echo "Examples" >> ../doc/${THETOOL}.dox
+    echo "========" >> ../doc/${THETOOL}.dox
+    echo "Some examples how to use ${THETOOL} can be found \\ref examples_${THETOOL} \"here\"" >> ../doc/${THETOOL}.dox
+    echo "FAQ" >> ../doc/${THETOOL}.dox
+    echo "========" >> ../doc/${THETOOL}.dox
+    echo "Frequently asked questions on ${THETOOL} can be found \\ref faq_${THETOOL} \"here\"" >> ../doc/${THETOOL}.dox
 done
 
 echo "create general dox file for aps list"
+echo "\section available_tools Available tools" > ../doc/apps.dox
 for file in ../src/apps/pk*.cc;do
     THETOOL=$(basename $file .cc)
     THESHORTDESCRIPTION=$(grep "${THETOOL}.cc: " $file | awk -v FS=':' '{print $2}')
     echo "- \\ref ${THETOOL} ${THESHORTDESCRIPTION}"; 
-done > ../doc/apps.dox
+done >> ../doc/apps.dox
 
 echo "Savannah repository for homepage can only be maintained via cvs"
 #mkdir ~/tmp
