7872221aad6bbb7cac8488e8649668e61bca225f
user <user@osgeolive.(none)>
2013-05-14 21:22:23 +0200

    removed vito related code from master

--- src/apps/pkgetchandelier.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PKGETCHANDELIER_
-#define _PKGETCHANDELIER_
-#include <math.h>
-#include <vector>
-
-double objFunction(const std::vector<double> &x, std::vector<double> &grad, void *my_func_data);
-#endif //_PKGETCHANDELIER_
