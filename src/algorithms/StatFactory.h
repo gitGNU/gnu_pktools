e78dab7fb9cc0cd964e4953a1d25e8714cfbc9f9
Pieter Kempeneers <kempenep@gmail.com>
2014-03-24 11:40:18 +0100

    value in percentiles in StatFactory based on maximum instead of median

--- src/algorithms/StatFactory.h
+++ src/algorithms/StatFactory.h
@@ -754,8 +754,10 @@ template<class T> void  StatFactory::percentiles (const std::vector<T>& input, t
       inputBin.push_back(*vit);
       ++vit;
     }
-    if(inputBin.size())
-      output[ibin]=median(inputBin);
+    if(inputBin.size()){
+      /* output[ibin]=median(inputBin); */
+      output[ibin]=max(inputBin);
+    }
   }
   if(!filename.empty()){
     std::ofstream outputfile;
