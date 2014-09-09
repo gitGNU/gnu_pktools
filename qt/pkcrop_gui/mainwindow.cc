2142c8c61f7e99eda3827ebf2037459ab8c66146
Pieter Kempeneers <kempenep@gmail.com>
2014-09-09 18:49:15 +0200

    documentation and pkcrop_gui error message

--- qt/pkcrop_gui/mainwindow.cc
+++ qt/pkcrop_gui/mainwindow.cc
@@ -23,6 +23,7 @@ along with pktools.  If not, see <http://www.gnu.org/licenses/>.
 #include <QFileDialog>
 #include <QProcess>
 #include <QMessageBox>
+#include <QProcess>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -262,12 +263,12 @@ void MainWindow::on_toolButton_Run_clicked()
         myProcess->start(program);
         myProcess->setProcessChannelMode(QProcess::MergedChannels);
         myProcess->waitForFinished(-1);
-        QString p_stderr = myProcess->readyReadStandardError();
-        if(!p_stderr.isEmpty()){
-            QMessageBox msgBox;
-            msgBox.setText(p_stderr);
-            msgBox.exec();
-        }
+//        QString p_stderr = myProcess->readyReadStandardError();
+//        if(!p_stderr.isEmpty()){
+//            QMessageBox msgBox;
+//            msgBox.setText(p_stderr);
+//            msgBox.exec();
+//        }
         QString p_stdout = myProcess->readAll();
         ui->consoleEdit->insertPlainText(p_stdout);
         delete myProcess;
