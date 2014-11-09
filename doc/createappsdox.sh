67c9950a8e610622cfc06aedbfce4ada5b8683e8
Pieter Kempeneers <kempenep@gmail.com>
2014-11-09 14:13:29 +0100

    added descriptions for utilities

--- doc/createappsdox.sh
+++ doc/createappsdox.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
 echo "create header"
-cat > ../doc/header.dox <<EOF
-\section thetool thetool
-theshortdescription
 
-## SYNOPSIS
-<code>
-thesynopsis
-</code>
+echo "create dox files for applications"
+for file in ../src/apps/pk*.cc;do    
+    THETOOL=$(basename $file .cc)
+    echo ${THETOOL}
 
-## DESCRIPTION ##
-thelongdescription
+    THESHORTDESCRIPTION=$(grep "${THETOOL}.cc: " $file | awk -v FS=':' '{print $2}')
+    THESYNOPSIS="${THETOOL} [OPTIONS]"
+    cat > ../doc/${THETOOL}.dox <<EOF
+\section $THETOOL $THETOOL
+$THESHORTDESCRIPTION
+
+EOF
+
+    #create description if not exists
+    touch ../doc/description_${THETOOL}.dox
+
+    cat ../doc/description_${THETOOL}.dox >> ../doc/${THETOOL}.dox
 
 ## OPTIONS ##
+    cat >> ../doc/${THETOOL}.dox <<EOF
+\section ${THETOOL}_options Options
  - use either \`-short\` or \`--long\` options (both \`--long=value\` and \`--long value\` are supported)
  - short option \`-h\` shows basic options only, long option \`--help\` shows all options
 |short|long|type|default|description|
 |-----|----|----|-------|-----------|
 EOF
 
-echo "create dox files for applications"
-for file in ../src/apps/pk*.cc;do    
-    THETOOL=$(basename $file .cc)
-    echo ${THETOOL}
-    cat ../doc/header.dox > ../doc/${THETOOL}.dox
-    sed -i "s/thetool/$THETOOL/g" ../doc/${THETOOL}.dox
-    THESHORTDESCRIPTION=$(grep "${THETOOL}.cc: " $file | awk -v FS=':' '{print $2}')
-    sed -i "s/theshortdescription/$THESHORTDESCRIPTION/" ../doc/${THETOOL}.dox
-    THESYNOPSIS="${THETOOL} [OPTIONS]"
-    sed -i "s/thesynopsis/$THESYNOPSIS/" ../doc/${THETOOL}.dox
-    THELONGDESCRIPTION="$THESHORTDESCRIPTION more..."
-    sed -i "s/thelongdescription/$THELONGDESCRIPTION/" ../doc/${THETOOL}.dox
     ${THETOOL} --doxygen|sed '$d' >> ../doc/${THETOOL}.dox
     echo >> ../doc/${THETOOL}.dox
     echo "Examples" >> ../doc/${THETOOL}.dox
